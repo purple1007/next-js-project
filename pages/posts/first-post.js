c57d9cbf747111e002e79ecab946674750376310
purple1007 <purple1007@gmail.com>
2020-08-27 00:33:36 +0800

    add Layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,28 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-    <h1>First Post</h1>
-    <h2>
-      <Link href="/">
-        <a>Back Home</a>
-      </Link>
-    </h2>
-    </>
+    <Layout>
+      <Head>
+        <title>第一篇文章</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back Home</a>
+        </Link>
+      </h2>
+      <style jsx>{`
+        h1{
+          color: #0070f3;
+        }
+        h2 a {
+          color: red;
+        }
+      `}
+      </style>
+    </Layout>
   )
 }
